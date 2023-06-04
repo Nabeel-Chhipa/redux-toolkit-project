fc9a4311a9f3eb503c3424ecb4fe35452a91c81d
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-06-04 19:05:12 +0500

    list movies with redux toolkit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,19 @@ import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
 import movieApi from '../../common/apis/movieApi'
 import { APIKEY } from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 
 const Home = () => {
+  
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const movieText = 'Harry'
     const fetchMovie = async () => {
       const res = await movieApi.get(`?apikey=${APIKEY}&s=${movieText}&type=movie`)
         .catch(err => console.log('Error ', err))
-        console.log('Movies ', res)
+        dispatch(addMovies(res.data))
     }
     fetchMovie()
   }, [])
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import './MovieListing.scss'
+import { useSelector } from 'react-redux'
+import { getAllMovies } from '../../features/movies/movieSlice'
+import MovieCard from '../MovieCard/MovieCard' 
 
 const MovieListing = () => {
+
+  const movies = useSelector(getAllMovies)
+  let rendermovies = ''
+
+  rendermovies = movies.Response === 'True' ?
+  movies.Search.map((movie, index) => (
+    <MovieCard key={index} data={movie} />
+  )) :
+  (
+    <div className='movies-error'>
+      <h3>{movies.Error}</h3>
+    </div>
+  )
+
   return (
-    <div>MovieListing</div>
+    <div className='movie-wrapper'>
+      <div className='movie-list'>
+        <h2>Movies</h2>
+        <div className='movie-container'>{rendermovies}</div>
+      </div>
+    </div>
   )
 }
 
